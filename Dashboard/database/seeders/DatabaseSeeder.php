5a1721ade0cff06e839d99ea5ebae8ef21ddb125
isaeel <pabloogtzz@gmail.com>
2026-02-20 14:15:15 -0600

    migraciones

--- Dashboard/database/seeders/DatabaseSeeder.php
+++ Dashboard/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +14,17 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    $this->call([
+        LoyaltyLevelSeeder::class,
+        BrandSeeder::class,
+        UserSeeder::class,
+        DriverSeeder::class,
+        CarSeeder::class,
+        RentalSeeder::class,
+        PaymentSeeder::class,
+    ]);
     }
 }
