5596d65937db1e9ee459fd67de4f4bfca70bf914
isaeel <pabloogtzz@gmail.com>
2026-04-18 16:48:33 -0600

    jale

--- Dashboard/app/Models/User.php
+++ Dashboard/app/Models/User.php
@@ -1,14 +1,15 @@
 <?php
-
+ 
 namespace App\Models;
-
+ 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
+ 
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject 
 {
     use Notifiable;
-
+ 
     protected $fillable = [
         'name',
         'email',
@@ -16,27 +17,43 @@ class User extends Authenticatable
         'loyalty_points',
         'loyalty_level_id'
     ];
-
+ 
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
+ 
     // Un usuario pertenece a un nivel
     public function loyaltyLevel()
     {
         return $this->belongsTo(LoyaltyLevel::class);
     }
-
+ 
     // Un usuario puede tener muchos conductores
     public function drivers()
     {
         return $this->hasMany(Driver::class);
     }
-
+ 
     // Un usuario puede tener muchas rentas
     public function rentals()
     {
         return $this->hasMany(Rental::class);
     }
+     /**
+     * Get the identifier that will be stored in the JWT token.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return an array with custom claims to be added to the JWT token.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
+ 
--- Dashboard/routes/api.php
+++ Dashboard/routes/api.php
@@ -1,28 +1,28 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\CarController;
+use App\Http\Controllers\Api\DriverController;
+use App\Http\Controllers\Api\RentalController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\LoyaltyLevelController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Api\AuthController;
 
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\DriverController;
-use App\Http\Controllers\RentalController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\BrandController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\LoyaltyLevelController;
-
+Route::apiResource('users', UserController::class);
+Route::apiResource('brands', BrandController::class);
 Route::apiResource('cars', CarController::class);
 Route::apiResource('drivers', DriverController::class);
 Route::apiResource('rentals', RentalController::class);
 Route::apiResource('payments', PaymentController::class);
-Route::apiResource('brands', BrandController::class);
-Route::apiResource('users', UserController::class);
 Route::apiResource('loyalty-levels', LoyaltyLevelController::class);
 
-// endpoints especiales 🔥
-Route::patch('cars/{id}/status', [CarController::class, 'updateStatus']);
-Route::patch('rentals/{id}/status', [RentalController::class, 'updateStatus']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('jwt')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::put('/user', [AuthController::class, 'updateUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
